f0081a451304370f1a5970a63c3e4e25ce7af06c
Natma Hired <nhired@fb.com>
2018-07-11 11:30:44 -0700

    enabled the ability to post one picture to main home feed

--- app/src/main/java/codepath/com/parsetagram/model/Post.java
+++ app/src/main/java/codepath/com/parsetagram/model/Post.java
@@ -11,6 +11,12 @@ public class Post extends ParseObject {
     private static final String KEY_DESCRIPTION = "description";
     private static final String KEY_IMAGE = "image";
     private static final String KEY_USER = "user";
+    private static final String KEY_OBJECT_ID = "objectId";
+
+    public Post() {
+        super();
+    }
+
 
     public String getDescription() {
         return getString(KEY_DESCRIPTION);
@@ -34,6 +40,9 @@ public class Post extends ParseObject {
 
     public void setUser(ParseUser user) {
         put(KEY_USER, user);
+    }
+
+    public  String getKeyObjectId() { return  KEY_OBJECT_ID;
     }
 
     public static class Query extends ParseQuery<Post> {
