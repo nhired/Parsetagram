c3d91a6a2e54a2c91b398f32818daa44e28cbf09
Natma Hired <nhired@fb.com>
2018-07-11 15:50:06 -0700

    recycler view now worksssss

---
+++ app/src/main/java/codepath/com/parsetagram/NewPost.java
@@ -0,0 +1,181 @@
+package codepath.com.parsetagram;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import codepath.com.parsetagram.model.Post;
+
+public class NewPost extends AppCompatActivity {
+
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    String objectId;
+
+    File photoFile;
+    private static File filesDir;
+    EditText text;
+    String caption;
+    ParseFile parseFile;
+
+    Button postBtn;
+    Post post;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        View view = findViewById(R.id.homeView);
+        filesDir = getApplicationContext().getFilesDir();
+        onLaunchCamera(view);
+
+        text = findViewById(R.id.caption_id);
+
+
+        postBtn = findViewById(R.id.post_btn);
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get the post object id so I can be able to query it
+               Intent data = new Intent();
+                caption = text.getText().toString();
+              // data.putExtra("object_id", post.getObjectId());
+
+               //get caption from edit text box to make it into a description
+
+               //data.putExtra("caption", text.getText().toString());
+
+               createPost(caption, parseFile, ParseUser.getCurrentUser());
+               setResult(RESULT_OK, data);
+               finish();
+            }
+        });
+    }
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(NewPost.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView ivPreview = (ImageView) findViewById(R.id.parsta_img);
+                ivPreview.setImageBitmap(takenImage);
+                photoFile = persistImage(takenImage, "test");
+
+                 parseFile = new ParseFile(photoFile);
+                //parseFile.saveInBackground();
+
+
+
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private static File persistImage(Bitmap bitmap, String name) {
+        File imageFile = new File(filesDir, name + ".jpg");
+
+        OutputStream os;
+        try {
+            os = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            Log.e("SaveImage", "Saving image did not work");
+        }
+
+        return imageFile;
+    }
+
+    private void createPost( String description, ParseFile imageFile, ParseUser user) {
+        post = new Post();
+        post.setDescription(description);
+        System.out.println("caption: "+ description);
+        post.setImage(imageFile);
+        post.setUser(user);
+        objectId = post.getObjectId();
+
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null) {
+                    Log.d("PostImage", "Post image successful");
+                    System.out.println("%%%%%%%%%%" + post.getObjectId());
+                } else {
+                    Log.e("PostImage", "Post image failed" );
+
+                }
+            }
+        });
+
+
+
+    }
+}
